d6a36f3400317c33228dd6777ccb49534f54bfd7
Ed Summers <ehs@pobox.com>
2016-01-03 20:59:27 -0500

    initial

---
+++ js/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+import { HydratorApp } from './components/HydratorApp.react';
+
+ReactDOM.render(
+  <HydratorApp />,
+  document.getElementById('app')
+);
+
---
+++ js/components/HydratorApp.react.js
@@ -0,0 +1,9 @@
+let React = require('react');
+
+class HydratorApp extends React.Component {
+  render() {
+    return <h1>Hello World!</h1>
+  }
+}
+
+export { HydratorApp };
